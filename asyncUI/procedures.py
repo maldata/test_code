2139ac34722542246eb88fb23ecec86ca5dd5b4d
maldata <maldata@gmail.com>
2017-02-07 21:39:41 -0600

    - Reorganized the classes a little

--- asyncUI/procedures.py
+++ asyncUI/procedures.py
@@ -1,107 +1,47 @@
-import tkinter
-import uuid
 import threading
 import queue
 import time
+
+
+class Result:
+    def __init__(self, handler=None, data=None):
+        self.data = data
+        self.handler = handler
 
 
 class Procedure(threading.Thread):
     def __init__(self, result_queue):
         super().__init__()
         self.result_queue = result_queue
-        self.mid_result = Result()
-        self.complete_result = Result()
 
     def enqueue_result(self, result, data=None):
-        r = Result(result.handler)
-        r.data = data
+        r = Result(result.handler, data)
         self.result_queue.put(r)
 
+    # This is the method you need to override.
     def run(self):
-        time.sleep(2)
+        """
+        This method needs to be overridden. It will execute when the
+        Procedure.start() method is called. While executing, you can
+        use the enqueue_result method to put Result objects in the queue.
+        The handler property of the Result will execute when the main
+        loop dequeues that Result.
+        """
+        raise NotImplementedError        
+
+   
+class DemoProcedure(Procedure):
+    def __init__(self, result_queue):
+        super().__init__(result_queue)
+        self.mid_result = Result()
+        self.end_result = Result()
+
+    def run(self):
+        print("Asynchronous thing is happening...")
+        time.sleep(1)
         self.enqueue_result(self.mid_result, 1)
-        time.sleep(2)
+        time.sleep(1)
         self.enqueue_result(self.mid_result, 2)
-        time.sleep(2)
+        time.sleep(1)
         self.enqueue_result(self.mid_result, 3)
-        self.enqueue_result(self.complete_result)
-        
-
-
-class Result:
-    def __init__(self, handler=None, data=None):
-        self._id = uuid.uuid1()
-        self.data = data
-        self.handler = handler
-
-
-class TkApp(tkinter.Tk):
-    def __init__(self, queue_time=0):
-        super().__init__()
-        self.queue_time = queue_time
-        self.result_queue = queue.Queue()
-        self.after(self.queue_time, self.check_queue)
-
-        self.p = Procedure(self.result_queue)
-        self.p.complete_result.handler = self.doAsyncThing
-        self.p.mid_result.handler = self.doAsyncThing
-        
-        
-    def check_queue(self):
-        if not self.result_queue.empty():
-            result = self.result_queue.get(block=False)
-            result.handler(result.data)
-        self.after(self.queue_time, self.check_queue)
-
-    def doThingNow(self):
-        print("Doing a thing RIGHT NOW!")
-
-    def doSlowThing(self):
-        print("Starting a slow thing.")
-        time.sleep(3)
-        print("Slow thing over.")
-
-    def doAsyncSlowThing(self):
-        print("Starting an asynchronous slow thing.")
-        self.p.start()
-        print("Asynchronous slow thing running in the background.")
-
-    def doAsyncThing(self, data):
-        if data is None:
-            print("All done!")
-        else:
-            print("Result is " + str(data))
-
-
-
-root = TkApp(queue_time=20)
-
-root.geometry("600x400+200+200")
-root.title("Procedure tester")
-
-frame = tkinter.Frame(root, borderwidth=2, relief=tkinter.SUNKEN)
-frame.pack(fill=tkinter.BOTH, side=tkinter.LEFT, expand=True)
-doNowButton = tkinter.Button(frame,
-                             text="Do Something Now",
-                             command=root.doThingNow)
-doNowButton.pack(fill=tkinter.X)
-doSlowThingButton = tkinter.Button(frame,
-                                   text="Do Something Slow",
-                                   command=root.doSlowThing)
-doSlowThingButton.pack(fill=tkinter.X)
-doAsyncSlowThingButton = tkinter.Button(frame,
-                                        text="Do Something Async",
-                                        command=root.doAsyncSlowThing)
-doAsyncSlowThingButton.pack(fill=tkinter.X)
-
-root.mainloop()
-
-"""
-API for a procedure:
-Create it
-Register results with functions to execute on return
-Start it
-Check the result queue in the main loop
-When there's a result in the queue, run the registered function
-
-"""
+        self.enqueue_result(self.end_result)
--- asyncUI/view.py
+++ asyncUI/view.py
@@ -1,3 +1,75 @@
-class View:
-    pass
+import procedures
+import tkinter
+import time
+import queue
 
+
+class BaseView(tkinter.Tk):
+    def __init__(self, queue_time=1):
+        super().__init__()
+        self.queue_time = queue_time
+        self.result_queue = queue.Queue()
+        self.after(self.queue_time, self.check_queue)
+        
+    def check_queue(self):
+        if not self.result_queue.empty():
+            result = self.result_queue.get(block=False)
+            result.handler(result.data)
+        self.after(self.queue_time, self.check_queue)
+
+
+class View:
+    def __init__(self):
+        self.root = BaseView(queue_time=10)
+        self.root.geometry("600x400+200+200")
+        self.root.title("Procedure tester")
+
+        frame = tkinter.Frame(self.root, borderwidth=2, relief=tkinter.SUNKEN)
+        frame.pack(fill=tkinter.BOTH, side=tkinter.LEFT, expand=True)
+        self.doNowButton = tkinter.Button(frame,
+                                          text="Do Something Now",
+                                          command=self.doThingNow)
+        self.doNowButton.pack(fill=tkinter.X)
+        self.doSlowThingButton = tkinter.Button(frame,
+                                                text="Do Something Slow",
+                                                command=self.doSlowThing)
+        self.doSlowThingButton.pack(fill=tkinter.X)
+        self.doAsyncSlowThingButton = tkinter.Button(frame,
+                                                     text="Do Something Async",
+                                                     command=self.doAsyncSlowThing)
+        self.doAsyncSlowThingButton.pack(fill=tkinter.X)
+
+        self.p = procedures.DemoProcedure(self.root.result_queue)
+        self.p.mid_result.handler = self.handle_result
+        self.p.end_result.handler = self.handle_result
+        
+    def runUI(self):
+        self.root.mainloop()
+
+    def doThingNow(self):
+        print("Doing a thing RIGHT NOW!")
+
+    def doSlowThing(self):
+        print("Starting a slow thing.")
+        time.sleep(3)
+        print("Slow thing over.")
+
+    def doAsyncSlowThing(self):
+        print("Starting an asynchronous slow thing.")
+        self.p.start()
+        print("Asynchronous slow thing started.")
+
+    def handle_result(self, data):
+        if data is None:
+            print("Done!")
+        else:
+            print("Result returned:" + str(data))
+            
+
+def main():
+    app = View()
+    app.runUI()
+
+if __name__=='__main__':
+    main()
+    
